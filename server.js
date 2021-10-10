1977d4e1614a6fe397ed93fc5beb8895ab6644ca
Joe Hertig <joehertig94@gmail.com>
2021-10-10 17:45:28 -0600

    incorporated a separate dataset

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
-const apiRoutes = require("./routes/apiRoutes");
-const htmlRoutes = require("./routes/htmlRoutes");
-
 const express = require("express");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+const apiRoutes = require("./routes/apiRoutes");
+const htmlRoutes = require("./routes/htmlRoutes");
 
-app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static("public"));
 
+// Use apiRoutes
 app.use("/api", apiRoutes);
 app.use("/", htmlRoutes);
 
